2d4a0c3d76395f43215e294caa0ed371c2f291d3
hotsuitor <hotsuitor@qq.com>
2022-02-27 11:05:18 +0800

    feat: minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use minigrep::{run, Config};
+use std::env;
+use std::process;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Search for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
